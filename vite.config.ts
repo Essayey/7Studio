0a0020247035657c344708499cbe224603bf263b
ArtemSarafanov <lucky84231@gmail.com>
2023-09-23 23:45:18 +0300

    history api fallback

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from '@svgr/rollup'
 import path from 'path'
+import history from 'connect-history-api-fallback'
 
 export default defineConfig({
     plugins: [react(), svgr()],
@@ -10,4 +11,5 @@ export default defineConfig({
             '@': path.resolve(__dirname, './src')
         },
     },
+    appType: 'spa',
 })
