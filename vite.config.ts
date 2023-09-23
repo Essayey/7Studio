575416bec2321752d7c11f73bba70862a1b508a1
ArtemSarafanov <lucky84231@gmail.com>
2023-09-23 23:46:34 +0300

    delete import

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from '@svgr/rollup'
 import path from 'path'
-import history from 'connect-history-api-fallback'
 
 export default defineConfig({
     plugins: [react(), svgr()],
